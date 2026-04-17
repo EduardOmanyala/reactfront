9e5e91da0853086735dab5b87ed8a6eaf43e99ca
Eduardo <bestessays001@gmail.com>
2026-04-17 20:54:30 +0300

    new

--- src/Config.js
+++ src/Config.js
@@ -1,7 +1,7 @@
 // src/config.js
-//const BASE_URL = "https://api.ken-lib.com"; // dev server
+const BASE_URL = "https://api.ken-lib.com"; // dev server
 //const BASE_URL = "http://127.0.0.1:8000";
-// When deploying, just switch to:
-const BASE_URL = "https://api.site.com";
+
+
 
 export default BASE_URL;
